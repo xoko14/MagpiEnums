9fc8f9d79a0ef9d55e74efd09f05a68c3e04b31b
xoko14 <xoquinperezb@gmail.com>
2023-11-30 11:56:35 +0100

    clean up

--- MagpiEnums.Example/Program.cs
+++ MagpiEnums.Example/Program.cs
@@ -1,26 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Text.Json;
-using MagpiEnums;
 using MagpiEnums.Example;
 
-[Enumeration]
-public abstract partial record TestEnumeration()
-{
-    
-}
 
-public static partial class Program
-{
-    public static void Main()
-    {
-        LoadStatus state = new LoadStatus.Loading("Loading...");
-        Console.WriteLine(JsonSerializer.Serialize(state));
+LoadStatus state = new LoadStatus.Loading("Loading...");
+Console.WriteLine(JsonSerializer.Serialize(state));
 
-        state = new LoadStatus.Loaded(new()
-        {
-            Title = "Content",
-            Description = "This is some loaded content."
-        });
-        Console.WriteLine(JsonSerializer.Serialize(state));
-    }
-}
+state = new LoadStatus.Loaded(new()
+{
+    Title = "Content",
+    Description = "This is some loaded content."
+});
+Console.WriteLine(JsonSerializer.Serialize(state));
